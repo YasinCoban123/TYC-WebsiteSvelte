d9ee4f908d75ac823d4c48d488ac6d8b6f62be33
YasinCoban123 <9018681@student.zadkine.nl>
2026-07-01 17:28:50 +0200

    fix: remove empty src

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -3,8 +3,7 @@
     <title>Home</title>
 </head>
 <section class="bg-white dark:bg-[#0f172a]">
-    <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-        <img class="w-full dark:hidden" src="" alt="dashboard image">
+    <div class="gap-8 items-center py-8 px-4 mx-auto max-w-7xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
         <img class="w-full hidden dark:block border-4 border-amber-500" src="/img/headbghome.jpeg" alt="header">
         <div class="mt-4 md:mt-0">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-[#FFF9E8]">Taha Yasin Coban, Programmeur.</h2>
